0b7f38d471d90ce5ba28eb879c7a976eaaf499ba
Katlego-ki <katlegobairvin@gmail.com>
2025-03-11 11:43:08 +0200

    added all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-//import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Layout from "./Pages/Layout"
+import Overview from "./Pages/Overview"
+import TechStack from "./Pages/TechStack"
+import Contact from "./Pages/Contact"
+import NoPage from "./Pages/NoPage"
+import Projects from "./Pages/Projects"
 import './App.css'
 
 function App() {
   
   return (
-    <>
-      <div className='container mx-auto'>
-        <h1 className='text-4xl text-center mt-10'>Hello Katlego</h1>
-      </div>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Overview />} />
+          <Route path="techstack" element={<TechStack />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="projects " element={<Projects />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   )
    
 }
---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Feel free to contact me at <a href="mailto:katlegobairvin@gmail.com">katlegobairvin@gmail.com</a></p>
+        </div>
+    )
+};
+
+export default Contact
