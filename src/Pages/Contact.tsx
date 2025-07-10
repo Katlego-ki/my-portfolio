ea32789070a9160669db02e0ea5f6c3a43fc96d0
Katlego-ki <katlegobairvin@gmail.com>
2025-07-10 16:46:20 +0200

    remove background on contact center text

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -3,8 +3,8 @@ const Contact = () => {
         <div className="grid opacity-90 h-[80dvh] w-9/10 md:w-3/4 mx-auto bg-black rounded-lg overflow-x-hidden">
             <h1 className="border-solid border-2 border-gray-400 text-center w-full h-fit rounded-md mx-auto text-[23px] font-mono">Contact:</h1>
             
-            <div className="animate-sl overflow-auto w-fit mx-auto wrapper">
-                <a className="grid shadow-sm shadow-gray-600 bg-gray-900 p-5 rounded-2xl m-auto text-xl text-center" href="mailto:katlegobairvin@gmail.com">Feel free to contact me at <span className="underline font-bold">katlegobairvin@gmail.com</span></a>
+            <div className="animate-sl overflow-auto w-fit mx-auto h-fit wrapper">
+                <h2 className="grid m-auto text-center">Feel free to contact me at <a className="underline font-bold" href="mailto:katlegobairvin@gmail.com">katlegobairvin@gmail.com</a></h2>
             </div>
         </div>
     )
