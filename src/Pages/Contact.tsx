723ce3c13d440ee4c062c31bdd200cf637be27d0
Katlego-ki <katlegobairvin@gmail.com>
2025-07-10 16:32:52 +0200

    fix scroll bar appearance on 'Contact'

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,10 +1,10 @@
 const Contact = () => {
     return (
-        <div className="flex-column justify-center bg opacity-90 h-[80dvh] w-9/10 md:w-3/4 mx-auto bg-black rounded-lg wrapper">
-            <h1 className="border-solid border-2 border-gray-400 text-center h-fit rounded-md mx-auto text-[23px] font-mono">Contact:</h1>
+        <div className="grid opacity-90 h-[80dvh] w-9/10 md:w-3/4 mx-auto bg-black rounded-lg overflow-x-hidden">
+            <h1 className="border-solid border-2 border-gray-400 text-center w-full h-fit rounded-md mx-auto text-[23px] font-mono">Contact:</h1>
             
-            <div className="animate-sl w-9/10 md:w-3/4 overflow-auto pb-[10vh] m-auto wrapper">
-                <h2 className="m-auto mt-[35vh] text-xl text-center">Feel free to contact me at <a className="underline font-bold" href="mailto:katlegobairvin@gmail.com">katlegobairvin@gmail.com</a></h2>
+            <div className="animate-sl overflow-auto w-fit mx-auto wrapper">
+                <a className="grid shadow-sm shadow-gray-600 bg-gray-900 p-5 rounded-2xl m-auto text-xl text-center" href="mailto:katlegobairvin@gmail.com">Feel free to contact me at <span className="underline font-bold">katlegobairvin@gmail.com</span></a>
             </div>
         </div>
     )
